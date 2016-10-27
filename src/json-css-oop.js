a01d34344d3abd41f2fff23e5accea2b14d33dae
muggy8 <muggy8@gmail.com>
2016-10-26 22:24:18 -0700

    working on oriented version of the wrapper

---
+++ src/json-css-oop.js
@@ -0,0 +1,56 @@
+(function(jsonCSS){
+	"use strict";
+	
+	jsonCSS.prototype.render = function(){
+		
+		// initialize the style renderer
+		var styleRenderer = document.querySelector("#json-css") || (function (){
+			var styleEle = document.createElement("style");
+			styleEle.setAttribute("id", "json-css");
+			document.querySelector("body").appendChild(styleEle);
+			return styleEle;
+		})();
+		
+		var buildStyle = function(mediaQuery, styleList){
+			
+			//type cast the media query into an array
+			if (typeof mediaQuery === "string" && mediaQuery){
+				mediaQuery = [mediaQuery];
+			}
+			
+			var ruleString = "";
+			// if media query exists
+			if (mediaQuery){
+				ruleString += "@media ";
+				mediaQuery.forEach(function(query, index, querySet){
+					if (index > 0){
+						ruleString += " and "
+					}
+					ruleString += "(" + query + ")";
+				});
+				ruleString += "{";
+			}
+			
+			styleList.forEach(function(cssRule, index, array){
+				//console.log(Object.keys(cssRule));
+				ruleString += cssRule.selector.toString() + "{";
+				Object.keys(cssRule.rules).forEach(function(ruleName, ruleIndex, ruleStylings){
+					ruleString += ruleName + ":" + cssRule.rules[ruleName] + ";";
+				});
+				ruleString += "}";
+			});
+			
+			if (mediaQuery){
+				ruleString += "}";
+			}
+			return ruleString;
+		};
+		
+		var renderedRuleset = "";
+		this.styleSet.forEach(function(querySet, index, JSONstyleSheet){
+			renderedRuleset += buildStyle(querySet.mediaQuery, querySet.ruleCluster);
+		});
+		
+		styleRenderer.innerHTML = renderedRuleset;
+	}
+})(window.jsonCSS || (window.jsonCSS = function(styles){this.jsonCSS.styleSet = styles || []}))
